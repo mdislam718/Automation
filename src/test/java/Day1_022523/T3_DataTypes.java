5bf5cbf65be2753589f033ad01736465b819d883
mdislam718 <130846934+mdislam718@users.noreply.github.com>
2023-04-15 11:23:04 -0400

    Initial commit

---
+++ src/test/java/Day1_022523/T3_DataTypes.java
@@ -0,0 +1,25 @@
+package Day1_022523;
+
+public class T3_DataTypes {
+
+    public static void main(String[] args) {
+
+        String message, numberString;
+        int number, number2;
+
+        //declare string data type
+        message = "Welcome to Automation class";
+        numberString = "1";
+
+        //declare int data type
+        number = 1;
+        number2 = 200;
+
+        System.out.println(message);
+        System.out.println(numberString);
+        System.out.println(number);
+        System.out.println(number2);
+
+    }//end of main method
+
+}// end of java class
